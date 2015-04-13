4a3b4d5f627316cee476a6e7dbe9e671ef14abc2
harjas <harjassinghglobal@gmail.com>
2015-04-12 23:37:48 -0500

    adding stuff

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ mongoose.connect('mongodb://harjas:mongo@ds033097.mongolab.com:33097/mp3');
 var allowCrossDomain = function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods',"GET, POST, PUT, OPTIONS, DELETE");
   next();
 };
 app.use(allowCrossDomain);
@@ -33,7 +34,7 @@ var port = process.env.PORT || 4000;        // set our port
 var router = express.Router();              // get an instance of the express Router
 
 router.use(function(req, res, next){
-	console.log("Logging constantly!");
+	//console.log("Logging constantly!");
 	next();
 });
 
@@ -50,19 +51,22 @@ var userRoute = router.route('/users');
 //----------USER ROUTE POST---------------
 userRoute.post(function(req, res){
 	var user = new User();
-	user.name = req.body.name;
-	user.email = req.body.email;
-
-	console.log(user.name);
-	console.log(user.email);
-
-	if(typeof user.name === 'undefined' && typeof user.email === 'undefined'){
-		res.json({message:"You're missing a field. Need name and email to create user!"});
+	if(typeof req.body.name === 'undefined' || typeof req.body.email === 'undefined'){
+		res.json({message:"You're missing a field. Need name and email to create user!", data:[]});
 	}
 	else{
-		User.find({email:user.email}, function(err,docs){
+		user.name = req.body.name;
+		user.email = req.body.email;
+		if(typeof req.body.pendingTasks !== "undefined"){
+			user.pendingTasks = req.body.pendingTasks;
+		}
+
+		//console.log(user.name);
+		//console.log(user.email);
+		User.find({"email":user.email}, function(err,docs){
 			if(docs.length){
-				res.json({message:"Email already exists"})
+				res.status(400);
+				res.json({message:"Email already exists", data:[]});
 			}
 			else{
 				user.save(function(err){
@@ -72,7 +76,10 @@ userRoute.post(function(req, res){
 					}
 
 					res.status(201);
-					res.json({message: 'User Created!'});
+					res.json({message: 'User Created!', data:user});
+					// User.find({email:user.email}, function(error,doc){
+					// 	res.json({message: 'User Created!', data:doc[0]});
+					// });
 				});
 			}
 		});
@@ -84,7 +91,6 @@ userRoute.get(function(req, res) {
 
 	var where = JSON.parse(req.query.where || "{}");
 	var fields = JSON.parse(req.query.select || "{}");
-	var attributes = ['_id', 'name', 'email', 'dateCreated', 'pendingTasks'];
 	var queryOptions = {
 		sort: JSON.parse(req.query.sort || "{}"),
 		skip: JSON.parse(req.query.skip || "{}"),
@@ -117,40 +123,59 @@ var indiUserRoute = router.route('/users/:id');
 
 indiUserRoute.get(function(req, res){
 	User.findById(req.params.id, function(err, user){
+		//console.log(user);
+		if(user === null){
+			res.status(404);
+			res.json({message:"User not found", data:[]});
+			return;
+		}
 		if(err){
 			res.status(404);
-			res.send(err);
+			res.json({message:"User not found", data:[]});
+			return;
 		}
 		res.status(200);
-		res.json(user);
+		res.json({message:"OK", data:user});
+		return;
 	});
 });
 //---------------------------------------------
-
 //-------PUT INDIVIDUAL USER INFO--------------
 indiUserRoute.put(function(req, res){
 	User.findById(req.params.id, function(err, user){
+		//console.log(user);
+		if(user === null){
+			//console.log("here");
+			res.status(404);
+			res.json({message:"User not found", data:[]});
+			return;
+		}
 		if(err){
 			res.status(404);
-			res.send(err);
+			res.json({message:"User not found", data:[]});
+			return;
+		}
+
+		if(typeof req.body.name === "undefined" || typeof req.body.email === "undefined"){
+			res.json({message:"You're missing a field. Need name and email to update user!", data:[]})
 		}
 
 		user.name = req.body.name;
 		user.email = req.body.email;
-
-		if(typeof user.name === "undefined" && typeof user.email === "undefined"){
-			res.json({message:"You're missing a field. Need name and email to update user!"})
+		if(typeof req.body.pendingTasks !== "undefined"){
+			user.pendingTasks = req.body.pendingTasks;
 		}
-		else{
-			user.save(function(err){
-				if(err){
-					res.status(500);
-					res.send(err);
-				}
-				res.status(200);
-				res.json({message:"updated user!"});
+		
+		user.save(function(err){
+			if(err){
+				res.status(500);
+				res.send(err);
+			}
+			res.status(200);
+			User.findById(req.params.id, function(error, updatedUser){
+				res.json({message:"Updated user!", data:updatedUser});
 			});
-		}
+		});
 	});
 });
 
@@ -160,12 +185,20 @@ indiUserRoute.delete(function(req, res){
 	User.remove({
 		_id: req.params.id
 	}, function(err, user){
+		//console.log(user);
+		if(user === 0){
+			//console.log("here");
+			res.status(404);
+			res.json({message:"User not found", data:[]});
+			return;
+		}
 		if(err){
 			res.status(404);
+			res.json({message:"User not found", data:[]});
 			res.send(err);
 		}
 		res.status(200);
-		res.json({message:"Successfully deleted user!"});
+		res.json({message:"Successfully deleted user!", data:user});
 	});
 });
 
@@ -178,21 +211,54 @@ indiUserRoute.delete(function(req, res){
 var taskRoute = router.route('/tasks');
 
 taskRoute.post(function(req, res){
-	var task = new Task();
-	task.name = req.body.name;
-	if(typeof req.body.description === "undefined"){
-		task.description = "";
-	}
-	else{
-		task.description = req.body.description;
-	}
-	task.deadline = req.body.date;
-	task.completed = false;
 
-	if(typeof task.name === "undefined" && typeof task.deadline === "undefined"){
-		res.json({message:"Task cannot be created without name or deadline!"});
-	}
-	else{
+	// if(!req.body.name || !req.body.deadline){
+	// 	res.json({message:"Task cannot be created without name or deadline!", data:[]});
+	// }
+
+	// var task = new Task(req.body);
+	// console.log(req.body.assignedUser);
+
+	// /*if(typeof task.name === "undefined" && typeof task.deadline === "undefined"){
+	// 	res.json({message:"Task cannot be created without name or deadline!", data:[]});
+	// }*/
+	// task.save();
+
+	// console.log(req.body);
+
+	// if(task.assignedUser === '') {
+	// 	return;
+	// }
+
+	// User.findById(task.assignedUser, function(err, user) {
+	// 	console.log(user.name)
+	// 	if(err) {
+	// 		res.status(404).json({message: "Not OK", data: "Didn't find user"});
+	// 	}
+	// 	else {
+	// 		user.pendingTasks.push(task._id);
+	// 		user.save();
+	// 		res.status(201).json({message: "OK", data: task});
+	// 	}
+	// });
+
+	var task = new Task();
+		task.name = req.body.name;
+		if(typeof req.body.description === "undefined"){
+			task.description = "";
+		}
+		else{
+			task.description = req.body.description;
+		}
+		if(typeof req.body.name === "undefined" && typeof req.body.deadline === "undefined"){
+			res.status(400);
+			res.json({message:"Task cannot be created without name or deadline!"});
+		}
+		task.deadline = req.body.deadline;
+		task.completed = false;
+		task.assignedUser = req.body.assignedUser;
+		task.assignedUserName = req.body.assignedUserName;
+
 		task.save(function(err){
 			if(err){
 				res.status(500);
@@ -200,9 +266,9 @@ taskRoute.post(function(req, res){
 			}
 
 			res.status(201);
-			res.json({message: 'Task Created!'});
+			res.json({message: 'Task Created!', data:task});
 		});
-	}
+	
 });
 
 taskRoute.get(function(req, res) {
@@ -220,9 +286,9 @@ taskRoute.get(function(req, res) {
         	res.status(404);
             res.send(err);
         }
-
+        console.log(tasks);
         res.status(200);
-        res.json(tasks);
+        res.json({message:"OK", data:tasks});
     });
 });
 
@@ -231,38 +297,73 @@ var indiTaskRoute = router.route('/tasks/:id');
 
 indiTaskRoute.get(function(req, res){
 	Task.findById(req.params.id, function(err, task){
+		if(task === null){
+			res.status(404);
+			res.json({message:"Task not found!", data:[]});
+			return;
+		}
 		if(err){
 			res.status(404);
-			res.send(err);
+			res.json({message:"Task not found!", data:[]});
+			return;
 		}
 		res.status(200);
-		res.json(task);
+		res.json({message:"OK",data:task});
 	});
 });
 
 indiTaskRoute.put(function(req, res){
+
 	Task.findById(req.params.id, function(err, task){
+		
+		if(task === null){
+			res.status(404);
+			res.json({message:"Task not found!", data:[]});
+			return;
+		}
+		
 		if(err){
 			res.status(404);
-			res.send(err);
+			res.json({message:"Task not found!", data:[]});
+			return;
 		}
 
-		task.name = req.body.name;	
-		task.email = req.body.email;
+		if(typeof req.body.name === "undefined" || typeof req.body.deadline === "undefined" || req.body.deadline === ""){
+			res.status(400);
+			res.json({message:"Cannot update task without name and deadline!", data:[]});
+			return;
+		}
+		if(typeof req.body.deadline !== "undefined"){
+			console.log("setting deadline");
+			task.deadline = req.body.deadline;	
+		}
+		if(typeof req.body.completed !== "undefined"){
+			console.log("setting completed");
+			task.completed = req.body.completed;	
+		}
 		
-		if(typeof task.name === "undefined" && typeof task.deadline === "undefined"){
-			res.json({message:"Task cannot be updated without name and deadline!"});
+		if(typeof req.body.name !== "undefined"){
+			task.name = req.body.name;	
 		}
-		else{
-			task.save(function(err){
-				if(err){
-					res.status(500);
-					res.send(err);
-				}
-				res.status(200);
-				res.json({message:"updated task!"});
-			});
+		if(typeof req.body.assignedUser !== "undefined"){
+			task.assignedUser = req.body.assignedUser;	
 		}
+		if(typeof req.body.assignedUserName !== "undefined"){
+			task.assignedUserName = req.body.assignedUserName;
+		}
+		
+		// if(typeof task.name === "undefined" && typeof task.deadline === "undefined"){
+		// 	res.json({message:"Task cannot be updated without name and deadline!", data:[]});
+		// }
+		task.save(function(err){
+			if(err){
+				res.status(500);
+				res.send(err);
+			}
+			console.log("saving this bad boy");
+			res.status(200);
+			res.json({message:"updated task!", data:task});
+		});
 	});
 });
 
@@ -270,12 +371,20 @@ indiTaskRoute.delete(function(req, res){
 	Task.remove({
 		_id: req.params.id
 	}, function(err, task){
+		//console.log(task);
+
+		if(task === 0){
+			res.status(404);
+			res.json({message:"Cannot delete task that doesnt exist!", data:[]});
+			return;
+		}
 		if(err){
 			res.status(404);
-			res.send(err);
+			res.json({message:"Cannot delete task that doesnt exist!", data:[]});
+			return;
 		}
 		res.status(200);
-		res.json({message:"Successfully deleted task!"});
+		res.json({message:"Successfully deleted task!", data:[]});
 	});
 });
 
@@ -283,3 +392,6 @@ app.use('/api', router);
 
 app.listen(port);
 console.log('Magic happens on port ' + port);
+
+
+
